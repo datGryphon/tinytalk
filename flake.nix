ca66818ef42f1d4279da353048c0ed94d71f79be
Caleb <calebwastler@gmail.com>
2026-09-10 16:20:56 -0400

    build: add TinyTAuK Python 3.13 dev shell

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "tinytalk - OpenAI-compatible NeuTTS server";
+  description = "tinytalk - OpenAI-compatible TTS server";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
@@ -8,20 +8,39 @@
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
       libPath = pkgs.lib.makeLibraryPath [ pkgs.stdenv.cc.cc.lib pkgs.zlib ];
+      mkDevShell = { python, venv, extras, backend }:
+        pkgs.mkShell {
+          packages = [ python pkgs.uv pkgs.ffmpeg-headless ];
+          shellHook = ''
+            export LD_LIBRARY_PATH="${libPath}''${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
+            export TINYTALK_BACKEND="${backend}"
+            if [ ! -d "${venv}" ]; then
+              uv venv "${venv}" --python ${python}/bin/python --python-preference only-system
+              uv pip install \
+                --python "${venv}/bin/python" \
+                --index-url https://download.pytorch.org/whl/cpu \
+                --extra-index-url https://pypi.org/simple \
+                -e '.[${extras}]'
+            fi
+            source "${venv}/bin/activate"
+          '';
+        };
     in
     {
       nixosModules.default = import ./nix/module.nix { inherit self; };
 
-      devShells.${system}.default = pkgs.mkShell {
-        packages = [ pkgs.python312 pkgs.uv pkgs.ffmpeg-headless ];
-        shellHook = ''
-          export LD_LIBRARY_PATH="${libPath}''${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
-          if [ ! -d .venv ]; then
-            uv venv --python python3 --python-preference only-system
-            uv pip install -e '.[test]'
-          fi
-          source .venv/bin/activate
-        '';
+      devShells.${system}.default = mkDevShell {
+        python = pkgs.python312;
+        venv = ".venv";
+        extras = "neutts,test";
+        backend = "neutts";
+      };
+
+      devShells.${system}.tinytauk = mkDevShell {
+        python = pkgs.python313;
+        venv = ".venv-tinytauk";
+        extras = "tinytauk,test";
+        backend = "tinytauk";
       };
     };
 }
